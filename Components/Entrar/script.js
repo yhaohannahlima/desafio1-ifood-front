8a266d6f660718510cb7e565ffa2347bfe8fa8cb
Eduardo377 <eduardogomes377@gmail.com>
2022-03-24 15:25:59 -0300

    pagina login minimamente completa com aviso de ero, funcao fake funcionando

--- Components/Entrar/script.js
+++ Components/Entrar/script.js
@@ -1,46 +1,61 @@
 let login = document.getElementById('login');
 
-export function logar() {
+function logar() {
     const senha = document.getElementById("password").value;
     const email = document.getElementById("email").value;
-    switch (senha || email) {
+    switch (senha && email) {
         case "":
-            return window.alert("Preencha todos os campos por gentieza.");
+            hidden();
+            break;
         case " ":
-            return window.alert("Preencha todos os campos por gentieza.");
+            hidden();
+            break;
         case null:
-            return window.alert("Preencha todos os campos por gentieza.");
+            hidden();
+            break;
         default:
             try {
-                if (email === "entregador@entrega.com" && senha === "1234") {
-                    window.alert("Login realizado com sucesso");
-                    window.location.href = "../ListaPedidos/index.html";
-                    // const corpo = {
-                    //     email: email,
-                    //     senha: senha
-                    // }
+                // if (email === "entregador@entrega.com" && senha === "1234") {
+                window.alert("Login realizado com sucesso");
+                window.location.href = "../ListaPedidos/index.html";
 
-                    // const cabecalho = {
-                    //     // method: "POST",
-                    //     body: JSON.stringify(corpo),
-                    //     headers: {
-                    //         "content-type": "application/json"
-                    //     }
-                    // }
+                // const corpo = {
+                //     email: email,
+                //     senha: senha
+                // }
 
-                    // fetch("http://localhost:8080/login")
-                    //     .then(res => res.json())
-                    //     .then(token => {
-                    //             window.alert("Login realizado com sucesso");
-                    // localStorage.setItem("login", token.token)
-                    //     window.location = "../ListaPedidos/index.html";
-                    // })
-                    // .catch(err => alert("Impossivel autenticar!"));
-                } else {
-                    return window.alert("Dados incorretos.")
-                }
+                // const cabecalho = {
+                //     // method: "POST",
+                //     body: JSON.stringify(corpo),
+                //     headers: {
+                //         "content-type": "application/json"
+                //     }
+                // }
+
+                // fetch("http://localhost:8080/login")
+                //     .then(res => res.json())
+                //     .then(token => {
+                //             window.alert("Login realizado com sucesso");
+                // localStorage.setItem("login", token.token)
+                //     window.location = "../ListaPedidos/index.html";
+                // })
+                // .catch(err => alert("Impossivel autenticar!"));
+                // } else {
+                //     return window.alert("Dados incorretos.")
+                // }
             } catch (error) {
-                return window.alert("Verifique as informações e tente novamente.")
+                hidden();
             }
     }
+}
+
+const hidden = () => {
+    alerta = document.querySelector('.alert');
+    alerta.classList.remove('hidden');
+
+    alerta.textContent = "Dados incorretos";
+
+    alerta.addEventListener('click', () => {
+        alerta.classList.add('hidden');
+    });
 }
