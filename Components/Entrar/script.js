5882f4cf69ad8100826440550145fbc6af140206
Eduardo377 <eduardogomes377@gmail.com>
2022-03-24 14:29:29 -0300

    funcao fake funcionando

--- Components/Entrar/script.js
+++ Components/Entrar/script.js
@@ -1,6 +1,6 @@
 let login = document.getElementById('login');
 
-export function logar() {
+function logar() {
     const senha = document.getElementById("password").value;
     const email = document.getElementById("email").value;
     switch (senha || email) {
@@ -12,33 +12,33 @@ export function logar() {
             return window.alert("Preencha todos os campos por gentieza.");
         default:
             try {
-                if (email === "entregador@entrega.com" && senha === "1234") {
-                    window.alert("Login realizado com sucesso");
-                    window.location.href = "../ListaPedidos/index.html";
-                    // const corpo = {
-                    //     email: email,
-                    //     senha: senha
-                    // }
+                // if (email === "entregador@entrega.com" && senha === "1234") {
+                window.alert("Login realizado com sucesso");
+                window.location.href = "../ListaPedidos/index.html";
+                // const corpo = {
+                //     email: email,
+                //     senha: senha
+                // }
 
-                    // const cabecalho = {
-                    //     // method: "POST",
-                    //     body: JSON.stringify(corpo),
-                    //     headers: {
-                    //         "content-type": "application/json"
-                    //     }
-                    // }
+                // const cabecalho = {
+                //     // method: "POST",
+                //     body: JSON.stringify(corpo),
+                //     headers: {
+                //         "content-type": "application/json"
+                //     }
+                // }
 
-                    // fetch("http://localhost:8080/login")
-                    //     .then(res => res.json())
-                    //     .then(token => {
-                    //             window.alert("Login realizado com sucesso");
-                    // localStorage.setItem("login", token.token)
-                    //     window.location = "../ListaPedidos/index.html";
-                    // })
-                    // .catch(err => alert("Impossivel autenticar!"));
-                } else {
-                    return window.alert("Dados incorretos.")
-                }
+                // fetch("http://localhost:8080/login")
+                //     .then(res => res.json())
+                //     .then(token => {
+                //             window.alert("Login realizado com sucesso");
+                // localStorage.setItem("login", token.token)
+                //     window.location = "../ListaPedidos/index.html";
+                // })
+                // .catch(err => alert("Impossivel autenticar!"));
+                // } else {
+                //     return window.alert("Dados incorretos.")
+                // }
             } catch (error) {
                 return window.alert("Verifique as informações e tente novamente.")
             }
