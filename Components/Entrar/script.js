8f4da6a8d7637662c4cc80ccc888901dcf01fde2
Eduardo377 <eduardogomes377@gmail.com>
2022-03-24 13:42:49 -0300

    Apanas a funcao de login em js

--- Components/Entrar/script.js
+++ Components/Entrar/script.js
@@ -1,9 +1,9 @@
 let login = document.getElementById('login');
 
-function logar() {
-    const password = document.getElementById("password").value;
+export function logar() {
+    const senha = document.getElementById("password").value;
     const email = document.getElementById("email").value;
-    switch (password || email) {
+    switch (senha || email) {
         case "":
             return window.alert("Preencha todos os campos por gentieza.");
         case " ":
@@ -11,25 +11,36 @@ function logar() {
         case null:
             return window.alert("Preencha todos os campos por gentieza.");
         default:
-            const corpo = {
-                email: email,
-                senha: password
-            }
-            const cabecalho = {
-                method: "GET",
-                body: JSON.stringify(corpo),
-                headers: {
-                    "content-type": "application/json"
-                }
-            }
-
-            fetch("http://localhost:8080/login", cabecalho)
-                .then(res => res.json())
-                .then(token => {
+            try {
+                if (email === "entregador@entrega.com" && senha === "1234") {
                     window.alert("Login realizado com sucesso");
-                    localStorage.setItem("login", token.token)
-                    window.location = "../ListaPedidos/index.html";
-                })
-                .catch(err => alert("Impossivel autenticar!"));
+                    window.location.href = "../ListaPedidos/index.html";
+                    // const corpo = {
+                    //     email: email,
+                    //     senha: senha
+                    // }
+
+                    // const cabecalho = {
+                    //     // method: "POST",
+                    //     body: JSON.stringify(corpo),
+                    //     headers: {
+                    //         "content-type": "application/json"
+                    //     }
+                    // }
+
+                    // fetch("http://localhost:8080/login")
+                    //     .then(res => res.json())
+                    //     .then(token => {
+                    //             window.alert("Login realizado com sucesso");
+                    // localStorage.setItem("login", token.token)
+                    //     window.location = "../ListaPedidos/index.html";
+                    // })
+                    // .catch(err => alert("Impossivel autenticar!"));
+                } else {
+                    return window.alert("Dados incorretos.")
+                }
+            } catch (error) {
+                return window.alert("Verifique as informações e tente novamente.")
+            }
     }
 }
