f0de164e65b031358941efa6175d2e1d11642438
Eduardo377 <eduardogomes377@gmail.com>
2022-03-25 22:14:14 -0300

    ajeitando a branch de trabalho

--- Components/Entrar/script.js
+++ Components/Entrar/script.js
@@ -1,8 +1,10 @@
 const login = document.getElementById('login');
-const urlLogin = "http://localhost:8080/login";​
+const urlLogin = "http://localhost:8080/login";
+console.log("Entrou " + login, urlLogin);
 async function logar() {
     const senha = document.getElementById("senha").value;
-    const email = document.getElementById("email").value;​
+    const email = document.getElementById("email").value;
+    console.log("antes do switch" + "\n" + senha + "\n" + email);
     switch (senha || email) {
         case "":
             hidden("Usuário e/ou senha incorretos!");
@@ -39,24 +41,24 @@ async function logar() {
                 hidden("Erro ao conectar!");
             }
     }
-};​
-
+};
 const hidden = (mensagem) => {
-        const alerta = document.querySelector('.alert');
-        alerta.classList.remove('hidden');​
-        alerta.textContent = mensagem;​
-        alerta.addEventListener('click', () => {
-            alerta.classList.add('hidden');
-        });
-    }
-    // fetch("http://localhost:8080/login")
-    //     .then(res => res.json())
-    //     .then(token => {
-    //             window.alert("Login realizado com sucesso");
-    // localStorage.setItem("login", token.token)
-    //     window.location = "../ListaPedidos/index.html";
-    // })
-    // .catch(err => alert("Impossivel autenticar!"));
-    // } else {
-    //     return window.alert("Dados incorretos.")
-    // }
+    const alerta = document.querySelector('.alert');
+    alerta.classList.remove('hidden');
+    alerta.textContent = mensagem;
+    alerta.addEventListener('click', () => {
+        alerta.classList.add('hidden');
+    });
+}
+
+// fetch("http://localhost:8080/login")
+//     .then(res => res.json())
+//     .then(token => {
+//             window.alert("Login realizado com sucesso");
+// localStorage.setItem("login", token.token)
+//     window.location = "../ListaPedidos/index.html";
+// })
+// .catch(err => alert("Impossivel autenticar!"));
+// } else {
+//     return window.alert("Dados incorretos.")
+// }
