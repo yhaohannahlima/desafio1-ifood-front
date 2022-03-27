91ac5b6f7d971844f52858b9698c9cbf4d3b72dd
Pam18 <yha.lima@outlook.com>
2022-03-26 21:08:38 -0300

    feat: popula a lista de pedidos com os dados da API

--- Componentes/ListaPedidos/script.js
+++ Componentes/ListaPedidos/script.js
@@ -1,80 +1,56 @@
+import { alerta } from "../util.js";
+
 const listaPedidos = document.querySelector('div .lista-pedidos');
-const linkApi = 'http://localhost:8080/pedidos/abertos/'; // adicionar link da api (http://localhost:8080/pedidos/abertos/)
 
+const linkApi = 'http://localhost:8080/pedidos/abertos';
 
-//lista fake
-const pedidos = [
-    { id: 1, situacao: "em aberto", cliente: "Josefin Faria" },
-    { id: 2, situacao: "finalizado", cliente: "Alberto Damagio" },
-    { id: 3, situacao: "em aberto", cliente: "Kenedy Triton" },
-    { id: 4, situacao: "em aberto", cliente: "Leandro Donato" },
-    { id: 5, situacao: "em aberto", cliente: "Merida Spoletto" },
-    { id: 6, situacao: "em aberto", cliente: "Nina Albuquerque" },
-    { id: 7, situacao: "finalizado", cliente: "Pietro Ferguncio" },
-    { id: 8, situacao: "finalizado", cliente: "Cora Coralina" },
-    { id: 9, situacao: "em aberto", cliente: "Fernando Pessoa" },
-    { id: 10, situacao: "finalizado", cliente: "Rubem Alves" },
-    { id: 11, situacao: "finalizado", cliente: "Jose Saraiva" },
-    { id: 12, situacao: "em aberto", cliente: "Dado Diamantrio" }
-];
-
-// inserirPedidos(pedidos); // deve ser retirado quando a API estiver fucnionando
 acessarListaDePedidosDoBancoDeDados(linkApi);
 
-//----------- FUNÇÕES
-function inserirPedidos(pedidos) {
-    if (pedidos.length === 0) {
-        return;
-    }
-
-    pedidos.forEach((item, indice) => {
-        novoPedido = document.createElement('button');
-        novoPedidoClasse = novoPedido.classList.add("btn");
-        novoPedidoClasse = novoPedido.classList.add("btn-pedido");
-
-        listaPedidos.append(novoPedido);
-
-        const pedido = document.querySelectorAll('.btn-pedido');
-        pedido[indice].textContent = `Código do pedido: ${item.id}`; // mudar para codigoPedido
-
-        pedido[indice].addEventListener('click', () => {
-            window.location.href = '../ConfirmarCancelar/index.html'; // mudar para IniciarPedido
-            localStorage.setItem('Dados do pedido', JSON.stringify(pedidos[indice]));
-        });
-    });
-}
-
-function acessarListaDePedidosDoBancoDeDados(linkApi) {
-    fetch(linkApi).then(function (response) {
+async function acessarListaDePedidosDoBancoDeDados(linkApi) {
+    await fetch(linkApi).then(function (response) {
         if (!response.ok) {
-            alerta = document.querySelector('.alert');
-            alerta.classList.remove('hidden');
-
-            alerta.textContent = "Não foi possível acessar a lista de pedidos!!!";
-
-            alerta.addEventListener('click', () => {
-                alerta.classList.add('hidden');
-            });
+            alerta(".alert-danger", "Não foi possível acessar a lista de pedidos!!!"); // colocar mensagem da API
 
             return;
         }
 
-        const promiseBody = response.json();
+        try {
+            const promiseBody = response.json();
 
-        promiseBody.then((body) => {
-            const listaPedidosAberto = [];
+            promiseBody.then((body) => {
+                body.forEach((item, indice) => {
+                    if (item.statusPedido != "aberto") {
+                        return;
+                    }
 
-            body.results.forEach(item => {
-                if (item.situacao != "em aberto") {
-                    return;
-                }
+                    if (indice === (body.length - 1)) {
+                        const carregando = document.querySelector('.spinner-border');
+                        const carregandoTexto = document.querySelector('.carregando-texto');
 
-                listaPedidosAberto.push(item);
+                        carregando.style.display = 'none';
+                        carregandoTexto.style.display = 'none';
+                        return;
+                    }
+
+                    const novoPedido = document.createElement('button');
+                    novoPedido.classList.add("btn");
+                    novoPedido.classList.add("btn-pedido");
+
+                    listaPedidos.append(novoPedido);
+
+                    const pedido = document.querySelectorAll('.btn-pedido');
+                    pedido[indice].textContent = `Código do pedido: ${item.codigoPedido}`;
+
+                    pedido[indice].addEventListener('click', () => {
+                        window.location.href = '../ConfirmarCancelar/index.html'; // mudar para IniciarPedido
+                        localStorage.setItem('Dados do pedido', JSON.stringify(pedidos[indice]));
+                    });
+
+                    // localStorage.setItem('Lista de Pedidos', listaPedidosAberto);
+                });
             });
-
-            inserirPedidos(listaPedidosAberto);
-
-            // localStorage.setItem('Lista de Pedidos', listaPedidosAberto);
-        })
+        } catch (error) {
+            return alerta(".alert-danger", error.message); // colocar mensagem da API
+        }
     });
-}
+};
---
+++ Componentes/util.js
@@ -0,0 +1,15 @@
+export function alerta(tipoAlerta, mensagem) {
+    const alerta = document.querySelector(tipoAlerta);
+
+    alerta.classList.remove('hidden');
+
+    alerta.textContent = mensagem;
+
+    alerta.addEventListener('click', () => {
+        alerta.classList.add('hidden');
+    });
+
+    setTimeout(() => {
+        alerta.classList.add('hidden');
+    }, 1000);
+}
