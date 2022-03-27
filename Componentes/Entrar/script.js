a607a8da433bcf2cb3d648ac9883d952879ba685
Pam18 <yha.lima@outlook.com>
2022-03-27 12:10:29 -0300

    feat: atualização da tela de entrar

---
+++ Componentes/Entrar/script.js
@@ -0,0 +1,62 @@
+const login = document.getElementById('login');
+const urlLogin = "http://localhost:8080/login";
+async function logar() {
+    const senha = document.getElementById("senha").value;
+    const email = document.getElementById("email").value;
+    switch (senha || email) {
+        case "":
+            hidden("Usuário e/ou senha incorretos!");
+            break;
+        case " ":
+            hidden("Usuário e/ou senha incorretos!");
+            break;
+        case null:
+            hidden("Usuário e/ou senha incorretos!");
+            break;
+        case undefined:
+            hidden("Usuário e/ou senha incorretos!");
+            break;
+        default:
+            try {
+                await fetch(urlLogin, {
+                    method: "POST",
+                    headers: {
+                        "Accept": "application/json",
+                        "content-type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        senha: senha
+                    })
+                }).then((response) => {
+                    if (response.status === 200) {
+                        window.location.href = '../ListaPedidos/index.html';
+                    } else {
+                        hidden("Usuário e/ou senha incorretos!");
+                    }
+                })
+            } catch (error) {
+                hidden("Erro ao conectar!");
+            }
+    }
+};
+
+const hidden = (mensagem) => {
+    const alerta = document.querySelector('.alert');
+    alerta.classList.remove('hidden');
+    alerta.textContent = mensagem;
+    alerta.addEventListener('click', () => {
+        alerta.classList.add('hidden');
+    });
+}
+    // fetch("http://localhost:8080/login")
+    //     .then(res => res.json())
+    //     .then(token => {
+    //             window.alert("Login realizado com sucesso");
+    // localStorage.setItem("login", token.token)
+    //     window.location = "../ListaPedidos/index.html";
+    // })
+    // .catch(err => alert("Impossivel autenticar!"));
+    // } else {
+    //     return window.alert("Dados incorretos.")
+    // }
