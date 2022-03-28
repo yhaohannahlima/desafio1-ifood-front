648acd93fa5da6911d96242fd8874dfed308a68f
Eduardo377 <eduardogomes377@gmail.com>
2022-03-28 11:34:38 -0300

    codigo a ser alterado

--- Components/IniciarPedido/script.js
+++ Components/IniciarPedido/script.js
@@ -4,31 +4,20 @@ const linkApi = 'http://localhost:8080/pedidos/abertos/'; // adicionar link da a
 
 //lista fake
 const pedidos = [
-    { id: 1, situacao: "em aberto", cliente: "Josefin Faria" },
-    { id: 2, situacao: "finalizado", cliente: "Alberto Damagio" },
-    { id: 3, situacao: "em aberto", cliente: "Kenedy Triton" },
-    { id: 4, situacao: "em aberto", cliente: "Leandro Donato" },
-    { id: 5, situacao: "em aberto", cliente: "Merida Spoletto" },
-    { id: 6, situacao: "em aberto", cliente: "Nina Albuquerque" },
-    { id: 7, situacao: "finalizado", cliente: "Pietro Ferguncio" },
-    { id: 8, situacao: "finalizado", cliente: "Cora Coralina" },
-    { id: 9, situacao: "em aberto", cliente: "Fernando Pessoa" },
-    { id: 10, situacao: "finalizado", cliente: "Rubem Alves" },
-    { id: 11, situacao: "finalizado", cliente: "Jose Saraiva" },
-    { id: 12, situacao: "em aberto", cliente: "Dado Diamantrio" }
+    { id: 1, situacao: "em aberto", cliente: "Josefin Faria" }
 ];
 
 // inserirPedidos(pedidos); // deve ser retirado quando a API estiver fucnionando
 acessarListaDePedidosDoBancoDeDados(linkApi);
 
 //----------- FUNÇÕES
-function inserirPedidos(pedidos) {
-    if (pedidos.length === 0) {
+function inserirPedido(pedido) {
+    if (pedido.length === 0) {
         return;
     }
 
-    pedidos.forEach((item, indice) => {
-        novoPedido = document.createElement('button');
+    pedido.forEach((item, indice) => {
+        novoPedido = document.createElement('.tela-tefone');
         novoPedidoClasse = novoPedido.classList.add("btn");
         novoPedidoClasse = novoPedido.classList.add("btn-pedido");
 
@@ -39,7 +28,7 @@ function inserirPedidos(pedidos) {
 
         pedido[indice].addEventListener('click', () => {
             window.location.href = '../ConfirmarCancelar/index.html'; // mudar para IniciarPedido
-            localStorage.setItem('Dados do pedido', JSON.stringify(pedidos[indice]));
+            localStorage.setItem('Dados do pedido', JSON.stringify(pedido[indice]));
         });
     });
 }
