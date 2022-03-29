580d19f5100f3fce99544e07629d9208f8e9779b
Eduardo377 <eduardogomes377@gmail.com>
2022-03-28 22:20:07 -0300

    Autenticacao com token pronta

--- Componentes/Entrar/script.js
+++ Componentes/Entrar/script.js
@@ -39,7 +39,9 @@ async function logar() {
                     if (resposta.status === 200) {
                         resposta.json()
                             .then((dadosResposta) => {
-                                localStorage.setItem("dadosResposta", dadosResposta.token);
+                                const idEntregador = parseJwt(dadosResposta.token);
+                                localStorage.setItem("token", dadosResposta.token);
+                                localStorage.setItem("idEntregador", idEntregador.sub);
                                 window.location.href = '../ListaPedidos/index.html'
                             })
                     } else {
@@ -51,4 +53,14 @@ async function logar() {
                 alerta(".alert-danger", "Erro ao conectar!");
             }
     }
+};
+
+// fonte : https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
 };
