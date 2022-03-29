2547bd616c106506206deef9f7fa09cff37d40a2
Eduardo377 <eduardogomes377@gmail.com>
2022-03-29 12:31:07 -0300

    trabalhando no codigo de funcao de iniciar corrida

--- Componentes/IniciarPedido/script.js
+++ Componentes/IniciarPedido/script.js
@@ -4,14 +4,20 @@
 //const linkApi = `${urlBase}/pedidos/aceitar/${pedidos.id}`; // adicionar link da api (http://localhost:8080/pedidos/abertos/)
 //const pontoInicial = [];
 //let idWatch;
-const idPedido = Number(pedido);
-const pedido = JSON.parse(localStorage.getItem('codigoPedido'));
-const idEntregador = JSON.parse(localStorage.getItem('idEntregador'));
+const pedidoString = localStorage.getItem('Dados do pedido');
+const pedidoObj = JSON.parse(pedidoString);
+const idPedido = Number(pedidoObj.codigoPedido);
+const latitudePedido = Number(pedidoObj.cliente.latitude);
+const longitude = Number(pedidoObj.cliente.longitude);
+const pontoInicialPedido = Number(pedidoObj.listaRastreio[0])
+const idEntregadorSrting = localStorage.getItem('idEntregador');
+const idEntregador = JSON.parse(idEntregadorSrting);
 const iniciarCorrida = document.querySelector('button .btn-iniciar');
 
 const pedidoNaTela = document.querySelector('div .card_pedido"');
-pedidoNaTela.textContent = `Pedido: ${pedidos.cliente[0]}`
+pedidoNaTela.textContent = `Pedido: ${pedidoObj.cliente.codigoCliente}`
 const nomeCliente = document.querySelector('div card_cliente');
+nomeCliente.textContent = `Cliente: ${pedidoObj.cliente.nome}`;
 
 
 iniciarCorrida.addEventListener(('click'), () => {
@@ -19,6 +25,6 @@ iniciarCorrida.addEventListener(('click'), () => {
 });
 
 function funcaoClicks() {
-    console.log(idPedido);
+    console.log(pedidoObj);
     console.log("Clicou");
 }
