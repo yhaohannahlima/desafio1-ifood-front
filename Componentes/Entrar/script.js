62de9c889d9c888e736db1e3a5e2d1ebaae1bdea
Eduardo377 <eduardogomes377@gmail.com>
2022-03-29 11:31:13 -0300

    trabalhando no codigo

--- Componentes/Entrar/script.js
+++ Componentes/Entrar/script.js
@@ -57,9 +57,9 @@ async function logar() {
 
 // fonte : https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
 function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
     return JSON.parse(jsonPayload);
